0695bcf841b12dfaa1fcf599f23be99c6f0dc6c5
10-Anas <anasq0q@gmail.com>
2022-03-24 22:33:11 -0700

    thu tasks

---
+++ THU24.3/functions/app.js
@@ -0,0 +1,249 @@
+`use strict`;
+function task(num) {
+    document.write(`task ${num} <br>`);
+}
+function hline() {
+    document.write(`<br> <hr>`);
+}
+//1
+task(1);
+
+  
+//   7
+//   Create a function called repeatWord
+//   that accept a string
+//   and return an object that represents how many times each word repeat
+//   ** no matter it is upper case or lower case
+//   ** Search on MDN about something can cut the string to words??
+
+// console.log(str.split(" "));
+//   Ex: repeatWord("My name is alex mercer class name B baba mama hello Hello HELLO")
+  function repeatword() {
+    str="My name is alex mercer class name B my baba mama hello Hello HELLO";
+    y=str.split(new RegExp(" ", "gi"));
+    x="";
+    sum=0;
+    for (let z = 0; z < y.length; z++) {
+          y[z];
+        for (let i = 0; i < y.length; i++) {
+            if (y[z] == y[i]) {
+                sum= sum + 1;
+    
+                  
+            }
+        }
+    return `${y[2]}:${sum}`;
+    }
+    
+}console.log(repeatword());
+document.write(repeatword());
+hline();
+//     str="My name is alex mercer class name B baba mama hello Hello HELLO";
+//     console.log(str.split( new RegExp( "my", "i"  ) ).length-1);
+//     console.log(str.split( new RegExp( "name", "i"  ) ).length-1);
+//     console.log(str.split( new RegExp( "is", "i"  ) ).length-1);
+//     console.log(str.split( new RegExp( "alex", "i"  ) ).length-1);
+//     console.log(str.split( new RegExp( "alex", "i"  ) ).length-1);
+//     console.log(str.split( new RegExp( "mercer", "i"  ) ).length-1);
+//     console.log(str.split( new RegExp( "class", "i"  ) ).length-1);
+//     console.log(str.split( new RegExp( "B", "i"  ) ).length-1);
+//     console.log(str.split( new RegExp( "baba", "i"  ) ).length-1);
+//     console.log(str.split( new RegExp( "mama", "i"  ) ).length-1);
+//     console.log(str.split( new RegExp( "hello", "i"  ) ).length-1);
+    
+//   
+  /*
+  => {
+    my:1,
+    name:2,
+    is:1,
+    alex:1,
+    mercer:1,
+    class:1,
+    b:1,
+    baba:1,
+    mama:1,
+    hello:3
+  }
+  */
+  
+  
+  
+  /*
+  8
+  Create a function called repeatChar
+  that accept a string
+  and return an object that represents how many times each char repeat
+  ** no matter it is upper case or lower case
+  ** Search on MDN about something can cut the string to char??
+  
+  Ex: repeatChar("mamababatetacedo")
+  => { m:2,  a:5, b:2, t2:, e:2, c:1, d:1, o:1}
+  */
+  
+  
+  /*
+  9
+  Create a function called selectFromObject
+  that accept an object and an array
+  and return an object have the key that inside the array
+  
+  Ex: selectFromObject({a: 1, cat: 3}, ['a', 'cat', 'd'])
+  =>  {a: 1, cat: 3}
+  */
+  task(3)
+
+  y={
+        name:"Anas",
+        age: 27,
+        skill:["html","css"],
+    }
+    x=Object.keys(y);
+    // console.log(y[x[2]]);
+    // document.write(y[x[2]]);
+
+
+    function selectFromObject() {
+        for (let i = 0; i < 3; i++) {
+            console.log(y[x[i]]); 
+        }
+    }
+    console.log(selectFromObject());
+    document.write(selectFromObject());
+
+
+    // document.write(selectFromObject());
+    hline();
+//   class FromObject{
+
+//     y(name,age,skill){
+//         this.name=name;
+//         this.age= age;
+//         this.skill=skill;
+//     }
+//    selectFromObject(x) {
+//        x=Object.keys(this.y);
+//     for (let i = 0; i < array.length; i++) {
+//     }
+//     return x ;
+//   }
+// }
+//   std= new FromObject("Duaa",24,["html","css"])
+//   console.log(std);
+  
+  /*
+  10
+  Create a function called objectToArray
+  that accept an object
+  and return an array of the keys and values in this object
+  
+  Ex: objectToArray({firstName:"Moh",age:24})
+  => ["firstName","Moh","age",24]
+  */
+  task(4)
+
+  y={
+        name:"Daua",
+        age: 24,
+        skill:["html","css","js"],
+    }
+    x=Object.keys(y);
+  function objectToArray() {
+    for (let i = 0; i < 3; i++) {
+        console.log(y[x[i]]); 
+    }  
+  }
+  console.log(objectToArray());
+  document.write(objectToArray())
+  hline();
+  
+  /*
+  11
+  Create a function called arrayToObject
+  that accept an array
+  and return an object of the keys and values in this object
+  
+  Ex: arrayToObject(["firstName","Moh","age",24])
+  => {firstName:"Moh",age:24}
+  */
+  task(5)
+  function arrayToObject(object) {
+    return Object.keys(object).reduce(function (r, k) {
+        object[k].forEach(function (a, i) {
+            r[i] = r[i] || {};
+            r[i][k] = a;
+        });
+        return r;
+    }, []);
+}
+
+  function getObject(array) {
+    return array.reduce(function (r, o, i) {
+        Object.keys(o).forEach(function (k) {
+            r[k] = r[k] || [];
+            r[k][i] = o[k];
+        });
+        return r;
+    }, {});
+}
+  let data={firstName:["Moh"],age:[24]};
+  console.log(getObject(arrayToObject(data)));
+  document.write(getObject(arrayToObject(data)));
+  /*
+  12
+  Create a function called onlyNumber
+  that accept an object
+  and return a new object that have only the values that is a number
+  **hint: search in MDN how to know the type of variable
+  
+  Ex: onlyNumber({firstName:"Moh",age:24,movies:[1,5,"string"]})
+  => {age:24}
+  */
+  
+
+  
+  /*
+  13
+  Create a function called onlyString
+  that accept an object
+  and return a new object that have only the values that is a string
+  **hint: search in MDN how to know the type of variable
+  
+  Ex: onlyString({firstName:"Moh",age:24,movies:[1,5,"string"]})
+  => {firstName:"Moh"}
+  */
+  
+  
+  /*
+  14
+  Create a function called onlyArray
+  that accept an object
+  and return a new object that have only the values that is a array
+  **hint: search in MDN how to know the type of variable
+  
+  Ex: onlyArray({firstName:"Moh",age:24,movies:[1,5,"string"]})
+  => {movies:[1,5,"string"]}
+  */
+  
+  
+  /*
+  15
+  Create a function called keysArray
+  that accept an object
+  and return an array have the key inside this object
+  
+  Ex: keysArray({firstName:"Moh",age:24,movies:[1,5,"string"]})
+  => ['firstName', 'age', 'movies']
+  
+  */
+  function getArray(object) {
+    return Object.keys(object).reduce(function (r, k) {
+        object[k].forEach(function (a, i) {
+            r[i] = r [i] || {};
+            r[i][k] = a;
+        });
+        return r;
+    }, []);
+}
+let data2={firstName:["Moh"],age:[24]};
+console.log(getArray(data2));
--- WED23.3/task5/app.js
+++ WED23.3/task5/app.js
@@ -1,15 +1,17 @@
 `use strict`
 let x=document.getElementById("pa1");
 let y=document.getElementById("pa2");
-// function ermsg1() {
+function ermsg1() {
     if (x.length<6)
-     {let erorr1=document.getElementById("sp1");erorr1.style.color="red";}
+     {let erorr1=document.getElementById("sp1")
+     erorr1.style.display="none";}
 
     else if (x.length===y.length)
-     {let erorr2=document.getElementById("sp2");erorr1.style.color="red";}
+     {let erorr2=document.getElementById("sp2")
+     erorr2.style.color="red";}
     
     else
-    {document.getElementsByTagName("button").style.display="block"}
+    {document.getElementsByTagName("button").style.display="block";}
 
-// }
+}
 
