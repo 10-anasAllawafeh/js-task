07b7909d5b64ca96cac8c6fb61d0d8cce08f2c16
10-Anas <anasq0q@gmail.com>
2022-03-27 14:03:39 -0700

    sunday tasks

---
+++ SUN27.3/mobileForm/app.js
@@ -0,0 +1,121 @@
+`use strict`
+let formDiv= document.getElementById("mobForm");
+let thElement=document.getElementById("headTa")
+let tbElement=document.getElementById("boadTa");
+
+////////////////////////////////////////////////////////////////////////////
+// let trElement = document.createElement("tr");
+// let tdElement4 = document.createElement("td4");
+// let tdElement1 = document.createElement("td1");
+// let tdElement2 = document.createElement("td2");
+// let tdElement3 = document.createElement("td3");
+
+// thElement.appendChild(trElement);
+// trElement.appendChild(tdElement4);
+// trElement.appendChild(tdElement1);
+// trElement.appendChild(tdElement2);
+// trElement.appendChild(tdElement3);
+
+// tdElement4.textContent = `Name`;
+// tdElement1.textContent = `type`;
+// tdElement2.textContent = `price`;
+// tdElement3.textContent = `condition`;
+/////////////////////////////////////////////////////////////////////////////
+
+let userfor=[];
+
+/////////////////////////////////////////////////////////////////////////////
+// function storeInLocalStorage(){
+//     let stringArray=JSON.stringify(userfor);
+//     localStorage.setItem("data",stringArray);
+// }
+
+// function callFromLocalStorage(){
+//     let dataObj=localStorage.getItem("data");
+//     // console.log(dataObj,"typeOf dataObj ",typeof dataObj);
+
+//     let arrayString=JSON.parse(dataObj);
+//     console.log(arrayString,"arrayString");
+//     if(arrayString != null){
+//         userfor=arrayString;
+//         // renderOrder(); 
+//     }
+//    renderOrder(); 
+// }
+// callFromLocalStorage();
+////////////////////////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////////////////////////
+function phone(useName,typeNameD){
+    this.useName=useName;
+    this.typeNameD=typeNameD;
+    this.price=priceMob(50,500);
+    this.cond=conditon();
+      
+
+    userfor.push(this);
+    renderType();
+    console.log(userfor);
+}
+////////////////////////////////////////////////////////////////////////////////
+function priceMob(min,max){
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function conditon() {
+    let price=phone.price;
+    if (price <= 100) {
+        return `used Mobile`;
+    } 
+    else {
+        return `new Mobile`;
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+formDiv.addEventListener("submit",handleSubmit);
+ function handleSubmit(event){
+    event.preventDefault();
+    // console.log(event.target);
+    let useName=event.target.useName.value;
+    // console.log(useName);
+    
+    let typeNameD=event.target.typeNameD.value;
+    // console.log(typeNameD);
+
+    new phone(useName,typeNameD);
+    console.log(phone);
+
+ }
+////////////////////////////////////////////////////////////////////////////////
+ handleSubmit();
+
+ ////////////////////////////////////////////////////////////////////////////////
+
+ ///////////////////////////////////////////////////////////////////////////////
+ function renderType() {
+    //  let tdElement1=document.getElementById("td1");
+    //  let tdElement2=document.getElementById("td2");
+    //  let tdElement3=document.getElementById("td3");
+    //  let tdElement4=document.getElementById("td4");
+    let trElement = document.createElement("tr");
+    let tdElement4 = document.createElement("td");
+    let tdElement1 = document.createElement("td");
+    let tdElement2 = document.createElement("td");
+    let tdElement3 = document.createElement("td");
+  
+    for (let i = 0; i < userfor.length; i++) {
+      tbElement.appendChild(trElement);
+      trElement.appendChild(tdElement4);
+      trElement.appendChild(tdElement1);
+      trElement.appendChild(tdElement2);
+      trElement.appendChild(tdElement3);
+  
+      tdElement4.textContent = userfor[i].useName;
+      tdElement1.textContent = userfor[i].typeNameD;
+      tdElement2.textContent = userfor[i].price;
+      tdElement3.textContent = userfor[i].cond;
+    }
+    //  return this.cusName+"has ordered "+this.drinkType
+  }
